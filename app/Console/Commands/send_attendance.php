7dda27a28776c1110f90282338dceb105e077ec3
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-07-29 14:56:47 +0800

    done

--- app/Console/Commands/send_attendance.php
+++ app/Console/Commands/send_attendance.php
@@ -47,12 +47,12 @@ class send_attendance extends Command
         if($attendance == null)
         {
             // $attendances = AttendanceLog::orderBy('id','asc')->where('location','!=','System')->get()->take(100);
-            $attendances = AttendanceLog::orderBy('id','asc')->where('location','Head Office')->get()->take(100);
+            $attendances = AttendanceLog::orderBy('id','asc')->where('location','Head Office')->get()->take(500);
         }
         else
         {
             // $attendances = AttendanceLog::where('id','>',$attendance->last_id)->where('location','!=','System')->orderBy('id','asc')->get()->take(100);
-            $attendances = AttendanceLog::where('id','>',$attendance->last_id)->where('location','Head Office')->orderBy('id','asc')->get()->take(100);
+            $attendances = AttendanceLog::where('id','>',$attendance->last_id)->where('location','Head Office')->orderBy('id','asc')->get()->take(500);
         }
         // info($attendances->toArray());
         // dd($attendances);
